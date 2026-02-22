a08bc74d31a68d2b4747b951bb1ed035e9ea4d71
dubstepping <arums.flower@gmail.com>
2026-02-21 18:47:51 -0600

    First commit

---
+++ internal/service/container.go
@@ -0,0 +1,16 @@
+package service
+
+import (
+	"bungleware/vault/internal/db"
+	"bungleware/vault/internal/storage"
+)
+
+type Service struct {
+	Projects ProjectService
+}
+
+func NewService(database *db.DB, storageAdapter storage.Storage) *Service {
+	return &Service{
+		Projects: NewProjectService(database, storageAdapter),
+	}
+}
